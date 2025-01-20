69492aea778e25e1a5b500e6d9f72736c93a631f
António Branco <ajsbranco15@gmail.com>
2025-01-20 17:06:47 +0000

    Add Burst
    Add initial burst iteration, including cooldown

--- GameJamSoftware2025/Assets/Scripts/Movement.cs
+++ GameJamSoftware2025/Assets/Scripts/Movement.cs
@@ -1,13 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float defaultSpeed = 5f;
-
+    [SerializeField] private float burstMultiplier = 1f;
     [SerializeField] private float rotateSpeed = 5f;
+    [SerializeField] private float burstCooldown = 2f;
+    [SerializeField] private Rigidbody2D playerRigidbody = default;
 
     private float rotationInput;
+    private float burstTimer = 0.0f;
 
     private void FixedUpdate()
     {
@@ -19,5 +23,23 @@ public class Movement : MonoBehaviour
     public void OnRotate(InputValue value)
     {
         rotationInput = value.Get<float>();
+    }
+    public void OnBurst()
+    { // WE might be able to fake force by just changing the sppeed
+        if (Time.time >= burstTimer)
+        {
+            Vector2 currentUp = transform.up;
+
+            currentUp.x += currentUp.x > 0 ? 1 : -1;
+            currentUp.y += currentUp.y > 0 ? 1 : -1;
+
+            playerRigidbody.AddForce(currentUp*burstMultiplier, ForceMode2D.Impulse);
+            burstTimer = burstCooldown + Time.time;
+            Debug.Log("BURSTED");
+        }
+        else
+        {
+            Debug.Log("FAILED burst");
+        }
     }
 }
